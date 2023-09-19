d8f5057c7b2bd1ffb28d5f6774a4df9d347829d1
dotGemini <diegohr91@gmail.com>
2023-09-19 18:10:20 -0300

    reset-pass

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+export default {
+    port:process.env.PORT,
+    mongo:process.env.MONGO_URL,
+    admin:process.env.ADMIN_NAME,
+    password:process.env.ADMIN_PASSWORD,
+    env: process.env.ENV,
+}
